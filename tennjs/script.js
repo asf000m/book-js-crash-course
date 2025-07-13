884b7994b12614b858928298031540d423940d25
asf000m <asf000m@gmail.com>
2025-07-12 23:03:36 -0300

    Finish tennjs game

--- tennjs/script.js
+++ tennjs/script.js
@@ -3,11 +3,19 @@ let ctx = canvas.getContext("2d");
 let width = canvas.width;
 let height = canvas.height;
 
-const BALL_SIZE = 5;
-let ballPosition = {x: 20, y:30};
+const MAX_COMPUTER_SPEED = 2;
 
-let xSpeed = 4;
-let ySpeed = 2;
+const BALL_SIZE = 5;
+let ballPosition;
+
+let xSpeed;
+let ySpeed;
+
+function initBall() {
+  ballPosition = {x: 20, y: 30};
+  xSpeed = 4;
+  ySpeed = 2;
+}
 
 const PADDLE_WIDTH = 5;
 const PADDLE_HEIGHT = 20;
@@ -16,7 +24,11 @@ const PADDLE_OFFSET = 10;
 let leftPaddleTop = 10;
 let rightPaddleTop = 30;
 
-let paddlesBallColors = "white"
+let paddlesBallColors = "white";
+
+let leftScore = 0;
+let rightScore = 0;
+let gameOver = false;
 
 document.addEventListener("mousemove", e => {
   rightPaddleTop = e.y - canvas.offsetTop;
@@ -46,11 +58,36 @@ function draw() {
     PADDLE_WIDTH,
     PADDLE_HEIGHT
   );
+
+  // draw scores
+  ctx.font = "30px monospace";
+  ctx.textAlign = "left";
+  ctx.fillText(leftScore.toString(), 50, 50);
+  ctx.textAlign = "right";
+  ctx.fillText(rightScore.toString(), width - 50, 50);
+}
+
+function followBall() {
+  let ball = {
+    top: ballPosition.y,
+    bottom: ballPosition.y + BALL_SIZE
+  };
+  let leftPaddle = {
+    top: leftPaddleTop,
+    bottom: leftPaddleTop + PADDLE_HEIGHT
+  };
+
+  if (ball.top < leftPaddle.top) {
+    leftPaddleTop -= MAX_COMPUTER_SPEED;
+  } else if (ball.bottom > leftPaddle.bottom) {
+    leftPaddleTop += MAX_COMPUTER_SPEED;
+  }
 }
 
 function update() {
   ballPosition.x += xSpeed;
   ballPosition.y += ySpeed;
+  followBall();
 }
 
 function checkPaddleCollision(ball, paddle) {
@@ -116,20 +153,41 @@ function checkCollision() {
     xSpeed = -Math.abs(xSpeed);
   }
 
-  if (ball.left < 0 || ball.right > width) {
-    xSpeed = -xSpeed;
+  if (ball.left < 0) {
+    rightScore++;
+    initBall();
+  }
+  if (ball.right > width) {
+    leftScore++;
+    initBall();
+  }
+  if (leftScore > 9 || rightScore > 9) {
+    gameOver = true;
   }
   if (ball.top < 0 || ball.bottom > height) {
     ySpeed = -ySpeed;
   }
 }
 
+function drawGameOver() {
+  ctx.fillStyle = "white";
+  ctx.font = "30px monospace";
+  ctx.textAlign = "center";
+  ctx.fillText("GAME OVER", width / 2, height / 2);
+}
+
 function gameLoop() {
   draw();
   update();
   checkCollision();
-
-  setTimeout(gameLoop, 15);   // call this function again after a timeout
+  if (gameOver) {
+    draw();
+    drawGameOver();
+  } else {
+    // call this function again after a timeout
+    setTimeout(gameLoop, 20);
+  }
 }
 
+initBall();
 gameLoop();
